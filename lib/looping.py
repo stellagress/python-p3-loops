d24e90cdebfb03814bbc3e663623e8d4c349683d
Stella Mazzillo <stellagress5@gmail.com>
2023-07-07 02:27:37 -0400

    compeltes python p3 loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 0:
+        print(i)
+        i -= 1
+    print('Happy New Year!')
+
+        # for i in range(10):
+        # i -= 1
+        # if i == 0:
+        #     print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i ** 2 for i in int_list]
+        
+
+
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+             print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
